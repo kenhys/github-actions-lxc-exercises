7c92756f7010cee72c9922fd2dadf0cac75bd1ed
Kentaro Hayashi <hayashi@clear-code.com>
2024-05-31 13:21:12 +0900

    Drop unknown target

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,10 +6,6 @@ echo "::group::Run test: list ubuntu images"
 lxc image list ubuntu:
 echo "::endgroup::"
 
-echo "::group::Run test: show info ubuntu"
-lxc image info ubuntu
-echo "::endgroup::"
-
 echo "::group::Run test: show info ubuntu:n"
 lxc image info ubuntu:n
 echo "::endgroup::"
