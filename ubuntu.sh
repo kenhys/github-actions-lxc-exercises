db6394f40a4e3a3ce95b4ce4d3ce528c808eade7
Kentaro Hayashi <hayashi@clear-code.com>
2024-05-31 14:39:16 +0900

    Show about noble

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,13 +2,26 @@
 
 set -ux
 
+echo "::group::Run test: show info ubuntu:n"
+lxc image info ubuntu:n
+echo "::endgroup::"
+
+echo "::group::Run test: show info ubuntu:24.04"
+lxc image info ubuntu:24.04
+echo "::endgroup::"
+
+echo "::group::Run test: show info 24.04"
+lxc image info 24.04
+echo "::endgroup::"
+
+echo "::group::Run test: show info noble"
+lxc image info noble
+echo "::endgroup::"
+
 echo "::group::Run test: list ubuntu images"
 lxc image list ubuntu:
 echo "::endgroup::"
 
-echo "::group::Run test: show info ubuntu:n"
-lxc image info ubuntu:n
-echo "::endgroup::"
 
 
 echo -e "\nAll check has passed!\n"
