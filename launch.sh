afd34a79ff0906dea00101f968c647ae5139a561
Kentaro Hayashi <hayashi@clear-code.com>
2024-05-31 14:23:11 +0900

    Collect information

--- launch.sh
+++ launch.sh
@@ -4,19 +4,27 @@ set -eux
 
 image=$1
 
+echo "::group::Run test: collect information"
+lxc launch $image info
+echo "::endgroup::"
+
 echo "::group::Run test: launch $image"
 lxc launch $image target
 sleep 5
 echo "::endgroup::"
+
 echo "::group::Run test: configure $image"
 lxc config device add target host disk source=$PWD path=/host
 lxc list
 echo "::endgroup::"
+
 echo "::group::Run test: show container info"
 lxc exec target -- /host/info.sh
 echo "::endgroup::"
+
 echo "::group::Run test: cleanup $image"
 lxc stop target
 lxc delete target
 echo "::endgroup::"
+
 echo -e "\nAll Success!\n"
