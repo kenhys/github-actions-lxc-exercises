7b51873653a99c6eb3dfb4a48a5aa2cfae15f8d6
Kentaro Hayashi <hayashi@clear-code.com>
2024-05-31 12:49:50 +0900

    Add check to list ubuntu:

--- test.sh
+++ test.sh
@@ -5,4 +5,9 @@ set -eux
 echo "::group::Run test: list images"
 lxc image list images:
 echo "::endgroup::"
+
+echo "::group::Run test: list ubuntu images"
+lxc image list ubuntu:
+echo "::endgroup::"
+
 echo -e "\nAll check has passed!\n"
