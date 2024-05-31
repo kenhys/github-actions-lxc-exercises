4b07b8ee00d1a371117fe5d1a2aa2735e725fd8e
Kentaro Hayashi <hayashi@clear-code.com>
2024-05-31 13:17:22 +0900

    Show more information

--- images.sh
+++ images.sh
@@ -6,4 +6,16 @@ echo "::group::Run test: list images"
 lxc image list images:
 echo "::endgroup::"
 
+echo "::group::Run test: show info images:centos/7"
+lxc image info images:centos/7
+echo "::endgroup::"
+
+echo "::group::Run test: show info images:amazonlinux/2"
+lxc image info images:amazonlinux/2
+echo "::endgroup::"
+
+echo "::group::Run test: show info images:amazonlinux/2023"
+lxc image info images:amazonlinux/2023
+echo "::endgroup::"
+
 echo -e "\nAll check has passed!\n"
--- ubuntu.sh
+++ ubuntu.sh
@@ -6,4 +6,13 @@ echo "::group::Run test: list ubuntu images"
 lxc image list ubuntu:
 echo "::endgroup::"
 
+echo "::group::Run test: show info ubuntu"
+lxc image info ubuntu
+echo "::endgroup::"
+
+echo "::group::Run test: show info ubuntu:n"
+lxc image info ubuntu:n
+echo "::endgroup::"
+
+
 echo -e "\nAll check has passed!\n"
