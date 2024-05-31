3b675ad552b8027a522c8386b43edf32de76ab48
Kentaro Hayashi <hayashi@clear-code.com>
2024-05-31 13:38:30 +0900

    Add launch more images

--- launch.sh
+++ launch.sh
@@ -2,18 +2,20 @@
 
 set -eux
 
-echo "::group::Run test: launch ubuntu 24.04"
-lxc launch ubuntu:n target
+image=$1
+
+echo "::group::Run test: launch $image"
+lxc launch $image target
 sleep 5
 echo "::endgroup::"
-echo "::group::Run test: configure image"
+echo "::group::Run test: configure $image"
 lxc config device add target host disk source=$PWD path=/host
 lxc list
 echo "::endgroup::"
 echo "::group::Run test: show container info"
 lxc exec target -- /host/info.sh
 echo "::endgroup::"
-echo "::group::Run test: cleanup ubuntu 24.04"
+echo "::group::Run test: cleanup $image"
 lxc stop target
 lxc delete target
 echo "::endgroup::"
