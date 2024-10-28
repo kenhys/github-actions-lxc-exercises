6e5f5a356764da2c7086c578284f71a4c7af4ae7
Kentaro Hayashi <hayashi@clear-code.com>
2024-10-28 12:20:32 +0900

    ci: add diagnostics.sh

---
+++ diagnostics.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+uname -a
+echo "::group::lxc ls"
+lxc ls
+echo "::endgroup::"
+echo "::group::snap info lxd"
+snap info lxd
+echo "::endgroup::"
+echo "::group::snap services lxd"
+snap services lxd
+echo "::endgroup::"
+echo "::group::snap logs lxd"
+snap logs lxd
+echo "::endgroup::"
