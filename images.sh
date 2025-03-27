0a2c35efeee878dcda5e23b1a3cae744bc71b200
Kentaro Hayashi <hayashi@clear-code.com>
2025-03-27 17:03:28 +0900

    Try to refer images.linuxcontainers.org

--- images.sh
+++ images.sh
@@ -18,4 +18,16 @@ echo "::group::Run test: show info images:amazonlinux/2023"
 lxc image info images:amazonlinux/2023
 echo "::endgroup::"
 
+echo "::group::Use images.linuxcontainers.org"
+lxc remote add linuxcontainers https://images.linuxcontainers.org
+echo "::endgroup::"
+
+echo "::group::Run test: show info linuxcontainers:amazonlinux/2"
+lxc image info linuxcontainers:amazonlinux/2
+echo "::endgroup::"
+
+echo "::group::Run test: show info linuxcontainers:amazonlinux/2023"
+lxc image info linuxcontainers:amazonlinux/2023
+echo "::endgroup::"
+
 echo -e "\nAll check has passed!\n"
