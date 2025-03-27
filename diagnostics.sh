4e4a8920b0668ec77cc1138efa7f87400241e92c
Kentaro Hayashi <hayashi@clear-code.com>
2025-03-27 16:56:52 +0900

    Add remote list

--- diagnostics.sh
+++ diagnostics.sh
@@ -4,6 +4,9 @@ uname -a
 echo "::group::lxc ls"
 lxc ls
 echo "::endgroup::"
+echo "::group::lxc remote list"
+lxc remote list
+echo "::endgroup::"
 echo "::group::snap info lxd"
 snap info lxd
 echo "::endgroup::"
